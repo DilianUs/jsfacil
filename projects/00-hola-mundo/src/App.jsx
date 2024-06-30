3ff0e75ec7b5f4ac715d1eb61b2f81528583223f
DilianUs <dilian0217@gmail.com>
2024-06-29 23:56:49 -0600

    tw card creates(only need create the action)

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,8 +1,23 @@
+import './App.css'
 export function App (){
-
     return(
-        <div>
-            <h1>Twitter card</h1>
-        </div>
+        <article className='tw-followCard'>
+            <header className="tw-followCard-header">
+                <img
+                className="tw-followCard-avatar" 
+                alt="Avatar de usuario" 
+                src="https://unavatar.io/DilianUs"/>
+                <div className="tw-followCard-userData">
+                    <strong className="tw-followCard-userName">Dilian Us Cachon</strong>
+                    <span className="tw-followCard-userAlias">@dilanus</span>
+                </div>
+            </header>
+            <aside className="tw-followCard-aside">
+                <button className="tw-followCard-followButton">
+                    Seguir
+                </button>
+            </aside>
+            
+        </article>
     )
 }
